bf284933eb4aed3cb5fc371b12c4ab75db033b34
josephtinkoy-boop <josephtinkoy@gmail.com>
2026-03-27 14:55:46 +0300

    span

--- src/components/Getproduct.jsx
+++ src/components/Getproduct.jsx
@@ -8,10 +8,10 @@ const Getproduct = () => {
   const [loading, setLoading] = useState("")
   const [error, setError] = useState("")
   const img_url = "http://josephtruham.alwaysdata.net/static/images/"
-  const navigate =useNavigate ()
+  const navigate = useNavigate()
 
   // function to fetch products from the database
-  const getproducts =async () => {
+  const getproducts = async () => {
     // updating the loading message
     setLoading("Please wait,we are retriving the products...")
     // connecting axios to flask api to fetch data from the database
@@ -19,38 +19,42 @@ const Getproduct = () => {
       const response = await axios.get("http://josephtruham.alwaysdata.net/api/get_product_details")
       setLoading("")
       setProducts(response.data)
-      
+
     } catch (error) {
       setLoading("")
       setError(error.message)
     }
   }
-  useEffect (() => {
+  useEffect(() => {
     getproducts()
   }, [])
   return (
     <div className='row'>
-     <h2>Available products</h2>
-     {loading}
-     {error} 
+      <h2>Available products</h2>
+      {loading}
+      {error}
 
-     {/* mapping the card to all the products */}
-     {products.map((product) =>(
-      <div className="col-md-3 justify-content-center mb-4">
-        <div className='card shadow'>
-        <img src={img_url + product.product_photo} alt="" className='product_img'/>
-        <div className="card-body">
-          <h5>{product.product_name}</h5>
-          <p>{product.product_description}</p>
-          <p>{product.product_cost}</p>
-          <button className='btn btn-dark mt-2 w-100' onClick={() =>navigate("/makepayment", {state:{product}})}>purchase now</button>
+      {/* mapping the card to all the products */}
+      {products.map((product) => (
+        <div className="col-md-3 justify-content-center mb-4">
+          <div className='card shadow'>
+            <img src={img_url + product.product_photo} alt="" className='product_img' />
+            <div className="card-body">
+              <h5>{product.product_name}</h5>
+              <p>{product.product_description}</p>
+              <p>{product.product_cost}</p>
+              <span class="badge bg-danger">New Brand</span>
+              <button className='btn btn-dark mt-2 w-100' onClick={() => navigate("/makepayment", { state: { product } })}>purchase now</button>
+            </div>
           </div>
         </div>
-      </div>
-     )
-    )
-    }
+
+
+      )
+      )
+      }
     </div>
+
   )
 }
 
