d4d55db9b77130438643d5cfdddeecbd83902a4d
josephtinkoy-boop <josephtinkoy@gmail.com>
2026-04-23 16:33:47 +0300

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ cart = [] }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="navbar navbar-dark bg-dark px-3">
+    <nav className="navbar navbar-dark bg-success px-3">
 
       <Link className="navbar-brand" to="/">
         Tinkoy Shop
